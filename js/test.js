ae1b6501e315ab99ba356caa5d57a6d9c068f103
Dongmuliang <dliangfun@gmail.com>
2017-10-07 02:42:15 -0500

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,7 +1,7 @@
 function m_test()
 {
   alert("abcdefg")
-id = document.getElementsByTagName('meta')['apple-itunes-app'].content.split('/')[3]
-window.name = "http://www.jianshu.com/notes/"+id+"/comments"
-alert(url)
+// id = document.getElementsByTagName('meta')['apple-itunes-app'].content.split('/')[3]
+// window.name = "http://www.jianshu.com/notes/"+id+"/comments"
+// alert(url)
 }
