5e2bf6d4f62c745762af27ac6100d10d6c6d80db
Dongmuliang <dliangfun@gmail.com>
2017-10-07 02:35:26 -0500

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,3 +1,3 @@
 id = document.getElementsByTagName('meta')['apple-itunes-app'].content.split('/')[3]
-url = "http://www.jianshu.com/notes/"+id+"/comments"
+window.name = "http://www.jianshu.com/notes/"+id+"/comments"
 alert(url)
